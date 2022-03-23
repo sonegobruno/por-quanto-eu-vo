fe3160713b9adfc7034b5b9806540acc698cf7e8
sonegobruno <sonegobruno@gmail.com>
2022-03-23 20:21:38 -0300

    feat: finished Gas price show page

--- src/screens/GasPriceShow/index.tsx
+++ src/screens/GasPriceShow/index.tsx
@@ -1,7 +1,23 @@
+import { useNavigation, useRoute } from '@react-navigation/native';
 import React from 'react';
 
 import * as S from './styles';
 
+type Params = {
+  amountCurrency: string;
+};
+
 export function GasPriceShow() {
-  return <S.Container />;
+  const route = useRoute();
+  const { amountCurrency } = route.params as Params;
+  const { goBack } = useNavigation<any>();
+
+  return (
+    <S.Container>
+      <S.Title>Você irá gastar em {'\n'} seu trajeto</S.Title>
+      <S.Amount>{amountCurrency}</S.Amount>
+      <S.BackPageText>Deseja somar novos valores?</S.BackPageText>
+      <S.BackButton title="Somar novo valor" onPress={goBack} />
+    </S.Container>
+  );
 }
--- src/screens/GasPriceShow/styles.ts
+++ src/screens/GasPriceShow/styles.ts
@@ -1,8 +1,51 @@
-import { SafeAreaView } from 'shared/components/SafeAreaView';
+import { Platform } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import styled from 'styled-components/native';
+import { Text } from 'react-native-elements';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import { SafeAreaView } from 'shared/components/SafeAreaView';
+import { PrimaryButton } from 'shared/components/Form/Buttons/PrimaryButton';
 
 export const Container = styled(SafeAreaView).attrs({
   omitEdges: ['top', 'bottom'],
 })`
+  padding: ${Platform.OS === 'ios' ? 0 : getStatusBarHeight()}px
+    ${RFValue(24)}px 0 ${RFValue(24)}px;
+
   flex: 1;
 `;
+
+export const Title = styled(Text)`
+  color: ${({ theme }) => theme.colors.neutral_500};
+  font-size: ${RFValue(24)}px;
+  line-height: ${RFValue(32)}px;
+  font-family: ${({ theme }) => theme.fonts.roboto_bold};
+  text-align: center;
+
+  margin-top: ${RFValue(40)}px;
+`;
+
+export const Amount = styled(Text)`
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: ${RFValue(40)}px;
+  font-family: ${({ theme }) => theme.fonts.roboto_bold};
+  text-align: center;
+
+  margin-top: ${RFValue(32)}px;
+`;
+
+export const BackPageText = styled(Text)`
+  color: ${({ theme }) => theme.colors.neutral_500};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.roboto_bold};
+  text-align: center;
+
+  margin-top: ${RFValue(56)}px;
+`;
+
+export const BackButton = styled(PrimaryButton).attrs({
+  containerStyle: {
+    marginTop: RFValue(32),
+  },
+})``;
