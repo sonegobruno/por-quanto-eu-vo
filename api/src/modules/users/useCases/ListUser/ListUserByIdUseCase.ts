be9da1059c732395cae531df2367d0bc6b094d8b
sonegobruno <sonegobruno@gmail.com>
2022-07-13 19:38:37 -0300

    feat: add user validator

--- api/src/modules/users/useCases/ListUser/ListUserByIdUseCase.ts
+++ api/src/modules/users/useCases/ListUser/ListUserByIdUseCase.ts
@@ -1,5 +1,6 @@
 // import { AppError } from '../../../../errors';
 import { inject, injectable} from 'tsyringe';
+import { AppError } from '../../../../errors';
 import { User } from '../../entities/User';
 import { IUsersRepository } from '../../repositories/IUserRepository';
 
@@ -12,7 +13,13 @@ class ListUserByIdUseCase {
     ){}
 
     public async execute(user_id: string): Promise<User> {
-        return this.userRepository.listUserById(user_id);
+      const user =  await this.userRepository.listUserById(user_id);
+
+      if(!user?.id) {
+        throw new AppError('Usuário não encontrado', 404)
+      }
+
+      return user
     }
 }
 
