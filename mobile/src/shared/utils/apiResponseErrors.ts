c6e35e8757738704daf623560ecbddf7c1c2b3eb
sonegobruno <sonegobruno@gmail.com>
2022-10-12 12:55:07 -0300

    feat: add errors type

--- mobile/src/shared/utils/apiResponseErrors.ts
+++ mobile/src/shared/utils/apiResponseErrors.ts
@@ -1,14 +1,34 @@
+import { AxiosError } from 'axios';
+
 interface ErrorResponse {
   message: string;
   field?: string;
 }
 
-export function apiResponseErrors(error: any): ErrorResponse {
-  console.error('Error ->', error);
-  if (error.response) {
-    return error.response.data as ErrorResponse;
+const IMPREVIOULY_ERROR: ErrorResponse = {
+  message:
+    'Erro não previsto pela aplicação. Por favor, contate o suporte para verificação.',
+};
+
+function hasKey<K extends string, T extends object>(
+  key: K,
+  object: T,
+): object is T & Record<K | 'field', string> {
+  return key in object;
+}
+
+export function apiResponseErrors(error: unknown): ErrorResponse {
+  if (error instanceof AxiosError) {
+    console.error('Axios Error ->', error);
+    const axiosError: AxiosError<object> = error;
+    console.error('Axios Error Response ->', axiosError.response);
+    if (!axiosError.response) return IMPREVIOULY_ERROR;
+    if (!hasKey('message', axiosError.response.data)) return IMPREVIOULY_ERROR;
+
+    return axiosError.response.data;
   }
 
+  console.error('Internal Error ->', error);
   return {
     message: 'Erro interno do sistema',
   };
